d8a288030bb9c36f7c5f867177577751dd1c4590
arielseptiadi <ariel.2233040073@mail.unpas.ac.id>
2025-10-23 20:32:06 +0700

    feat: Add Manajement Layanan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import RegisterClientPage from './pages/RegisterClientPage'
-import RegisterFreelancerPage from './pages/RegisterFreelancerPage'
-import DashboardPage from './pages/DashboardPage'
-import AdminDashboardPage from './pages/AdminDashboardPage'
-import ProfilePage from './pages/ProfilePage'
-import ProtectedRoute from './components/templates/ProtectedRoute'
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterClientPage from "./pages/RegisterClientPage";
+import RegisterFreelancerPage from "./pages/RegisterFreelancerPage";
+import DashboardPage from "./pages/DashboardPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProtectedRoute from "./components/templates/ProtectedRoute";
+import ServicePage from "./pages/freelance/ServicePage";
 
 export default function App() {
   return (
@@ -13,12 +14,39 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register/client" element={<RegisterClientPage />} />
       <Route path="/register/freelancer" element={<RegisterFreelancerPage />} />
-      <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
-      <Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboardPage /></ProtectedRoute>} />
-      <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute>
+            <AdminDashboardPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/freelance/service"
+        element={
+          <ProtectedRoute>
+            <ServicePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
-  )
+  );
 }
-
-
