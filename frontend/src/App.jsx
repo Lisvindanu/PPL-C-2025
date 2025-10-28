d009b369102dbba70d04872d947c034b274f3792
miftt <miftahfauzi012@gmail.com>
2025-10-28 13:09:08 +0700

    Add Order and Service Pages
    - Introduced new pages for managing orders: `OrderListPage`, `OrderDetailPage`, and `CreateOrderPage`.
    - Added `ServiceListPage` and `ServiceDetailPage` for browsing and viewing services.
    - Integrated routing for the new pages within the main application.
    - Removed the obsolete `BookmarkPage` and `OrderPage` components to streamline the codebase.
    - Enhanced the application structure to support order and service management functionalities.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import AdminDashboardPage from "./pages/AdminDashboardPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRoute from "./components/templates/ProtectedRoute";
 import ServicePage from "./pages/freelance/ServicePage";
+import OrderDetailPage from "./pages/OrderDetailPage";
+import OrderListPage from "./pages/OrderListPage";
+import ServiceListPage from "./pages/ServiceListPage";
+import ServiceDetailPage from "./pages/ServiceDetailPage";
+import CreateOrderPage from "./pages/CreateOrderPage";
 
 export default function App() {
   return (
@@ -45,6 +50,46 @@ export default function App() {
         element={
           <ProtectedRoute>
             <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/orders/:id"
+        element={
+          <ProtectedRoute>
+            <OrderDetailPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/orders"
+        element={
+          <ProtectedRoute>
+            <OrderListPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/orders/create"
+        element={
+          <ProtectedRoute>
+            <CreateOrderPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/services"
+        element={
+          <ProtectedRoute>
+            <ServiceListPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/services/:id"
+        element={
+          <ProtectedRoute>
+            <ServiceDetailPage />
           </ProtectedRoute>
         }
       />
